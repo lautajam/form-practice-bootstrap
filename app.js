51cba6994945c500eed8974855b9ff8625e49ba9
lautajam <lautajamdev@gmail.com>
2022-08-22 20:59:07 -0300

    I added the toast with user data and I style the index

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const formSubmit = document.querySelector("#formSubmit");
+const btnSubmit = document.querySelector("#btn-submit");
+const btnLoading = document.querySelector("#btn-loading");
+const toast = document.querySelector(".toast");
+var userData = document.querySelector("#user-data")
 
 formSubmit.addEventListener("submit", e =>{
     e.preventDefault();
+
+    btnSubmit.classList.add("d-none");
+    btnLoading.classList.remove("d-none");
+
     const data = new FormData(formSubmit);
     console.log("Email area: ", data.get("emailArea"));
     console.log("Passwrod area: ", data.get("pswrdArea"));
     console.log("Checkbox area: ", data.get("checkArea"));
+
+    var emailArea = data.get("emailArea");
+    var pswrdArea = data.get("pswrdArea");
+
+    window.setTimeout(()=>{
+        btnSubmit.classList.remove("d-none");
+        btnLoading.classList.add("d-none");
+
+        const eventToast =  new bootstrap.Toast(toast);
+        userData.innerHTML += "Email: " + emailArea + "<br>";
+        userData.innerHTML += "Password: " + pswrdArea;
+        eventToast.show();
+
+    },3000);
+
     formSubmit.reset();
 });
 
